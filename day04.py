2ba0caca494ca950a8abb43a6b49f29293f36ff0
LysanderGG <lysandergc@gmail.com>
2016-12-26 17:06:18 +0900

    day04 - remove unused variable and replace a list construction with a generator
    Signed-off-by: LysanderGG <lysandergc@gmail.com>

--- day04.py
+++ day04.py
@@ -5,15 +5,13 @@ from operator import itemgetter
 
 
 def read_room_list(filepath):
-    room_list = []
     with open(filepath) as f:
         for line in f:
             m = re.search("([a-z\-]+)(\d+)\[(\w+)\]", line)
             name = ''.join([c for c in m.group(1) if c != '-'])
             number = int(m.group(2))
             checksum = m.group(3)
-            room_list.append((name, number, checksum))
-    return room_list
+            yield (name, number, checksum)
 
 
 def get_room_name(room): return room[0]
@@ -41,7 +39,6 @@ def rotate_letter(l, i):
 
 
 def find_secret_room(room_list, secret):
-    res = ""
     for room in room_list:
         room_id = get_room_id(room)
         clear_name = ''.join([rotate_letter(c, room_id) for c in get_room_name(room)])
@@ -49,7 +46,7 @@ def find_secret_room(room_list, secret):
             print(room)
 
 
-room_list = read_room_list("day04.txt")
+room_list = list(read_room_list("day04.txt"))
 print("Part 1:", get_sum_real_rooms(room_list))
 print("Part 2:")
 find_secret_room(room_list, "northpole")
