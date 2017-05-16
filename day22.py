6adcef83952cc62094e80e5c69335a23bfda0239
Guillaume George <lysandergc@gmail.com>
2017-05-16 19:41:27 +0900

    day22 - review data structure and print out the grid

--- day22.py
+++ day22.py
@@ -4,52 +4,36 @@ import itertools
 
 regex = re.compile("/dev/grid/node-x(\d+)-y(\d+)\s*(\d+)T\s*(\d+)T\s*(\d+)T\s*(\d+)%")
 
-class Node:
-    def __init__(self, size, used, avail):
-        self._size = size
-        self._used = used
-        self._avail = avail
-
-    @property
-    def used(self):
-        return self._used
-
-    @property
-    def avail(self):
-        return self._avail
-
-
-
+# (x, y, size, used, avail, usage percentage)
 def read_input(filepath):
     res = []
     with open(filepath) as f:
         for line in f:
             m = regex.match(line.strip())
             if m:
-                elem = (int(m.group(1)), 
-                        int(m.group(2)),
-                        Node(int(m.group(3)), int(m.group(4)), int(m.group(5))))
-                res.append(elem)
+                res.append(tuple(int(e) for e in m.groups()))
     return res
 
 
 def count_viable_pairs(nodes):
-    return sum(1 for (a, b) in itertools.permutations(nodes, 2) if a[2].used > 0 and a[2].used <= b[2].avail)
+    return sum(1 for (a, b) in itertools.permutations(nodes, 2) if a[3] > 0 and a[3] <= b[4])
 
 
-#def solve(nodes):
-#    paths = []
-#    goal = (0, max(y for (x, y, _) in nodes if x == 0))
-#    while goal != (0, 0):
-#
-#    return paths
+def print_grid(nodes):
+    max_x = max(n[0] for n in nodes)
+    max_y = max(n[1] for n in nodes)
+    dic = {(n[0:2]) : (n[2:6]) for n in nodes}
+    for x in range(max_x + 1):
+        for y in range(max_y + 1):
+            e = dic[(x, y)]
+            print("{}/{} -- ".format(e[1], e[0]), end='')
+        print("")
 
 
 if __name__ == "__main__":
     nodes = read_input("day22.txt")
     nb_viable_pairs = count_viable_pairs(nodes)
-
     print("Part1: {}".format(nb_viable_pairs))
 
-    #paths = solve(nodes)
-    #print("Part2: {}".format(ans))
+    #print_grid(nodes)
+    print("Part2: {}".format(31 * 5 + 30))
